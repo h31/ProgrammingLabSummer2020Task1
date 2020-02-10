824ca70a11ebf71729fb3fe87ab9ce6613ec73da
OGSegu <seguisreal@gmail.com>
2020-02-10 23:27:47 +0300

    Added: AddressBook class.

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddressBook {
+    private HashMap<String, Address> map = new HashMap<>();
+
+    public void addPerson(String surname, Address address) {
+        map.put(surname, address);
+    }
+
+    public void removePerson(String surname) {
+        map.remove(surname);
+    }
+
+    public void changeAdds(String surname, Address address) {
+        map.replace(surname, address);
+    }
+
+    public Address getAdds(String surname) {
+        return map.get(surname);
+    }
+
+    public ArrayList<String> filterStreet(String street) {
+        ArrayList<String> people = new ArrayList<>();
+        for (Map.Entry<String, Address> element: this.map.entrySet()) {
+            if (element.getValue().getStreet().equals(street)) people.add(element.getKey());
+        }
+        return people;
+    }
+
+    public ArrayList<String> filterHouse(String house) {
+        ArrayList<String> people = new ArrayList<>();
+        for (Map.Entry<String, Address> element: this.map.entrySet()) {
+            if (element.getValue().getHouse().equals(house)) people.add(element.getKey());
+        }
+        return people;
+    }
 }
 
 class Address {
