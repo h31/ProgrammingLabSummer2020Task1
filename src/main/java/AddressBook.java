5a6f5214bace5ddbe24771a7d4f26abb525bc4e9
OGSegu <seguisreal@gmail.com>
2020-02-10 23:27:17 +0300

    Added: Address class.

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,2 +1,23 @@
 public class AddressBook {
 }
+
+class Address {
+    private String street;
+    private String house;
+    private String apartment;
+
+    public Address(String street, String house, String apartment) {
+        this.street = street;
+        this.house = house;
+        this.apartment = apartment;
+    }
+    String getStreet() {
+        return this.street;
+    }
+    String getHouse() {
+        return this.house;
+    }
+    String getApartment() {
+        return this.apartment;
+    }
+}
