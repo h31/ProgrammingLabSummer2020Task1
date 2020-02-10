1374457a8aaf4ed60454966c962fab3aed7f6a2e
Br0mm <Andrew-Ost@mail.ru>
2020-02-11 02:19:44 +0300

    добавил поворот задней и нижней сторон на 90 градусов по часовой стрелке

--- src/MyProject/Cube.java
+++ src/MyProject/Cube.java
@@ -20,6 +20,33 @@ public class Cube {
     private static String[][] up = {{WHITE + "▮", WHITE + "▮"}, {WHITE + "▮", WHITE + "▮"}};
     private static String[][] down = {{YELLOW + "▮", YELLOW + "▮"}, {YELLOW + "▮", YELLOW + "▮"}};
 
+    public static void main (String[] args) {
+        System.out.println("Front: ");
+        System.out.println(front[0][0] + front[0][1]);
+        System.out.println(front[1][0] + front[1][1]);
+        System.out.println("UP: ");
+        System.out.println(up[0][0] + up[0][1]);
+        System.out.println(up[1][0] + up[1][1]);
+        d();
+        System.out.println("UP: ");
+        System.out.println(up[0][0] + up[0][1]);
+        System.out.println(up[1][0] + up[1][1]);
+        System.out.println("Front: ");
+        System.out.println(front[0][0] + front[0][1]);
+        System.out.println(front[1][0] + front[1][1]);
+        System.out.println("Down: ");
+        System.out.println(down[0][0] + down[0][1]);
+        System.out.println(down[1][0] + down[1][1]);
+        System.out.println("Back: ");
+        System.out.println(back[0][0] + back[0][1]);
+        System.out.println(back[1][0] + back[1][1]);
+        System.out.println("Left: ");
+        System.out.println(left[0][0] + left[0][1]);
+        System.out.println(left[1][0] + left[1][1]);
+        System.out.println("Right: ");
+        System.out.println(right[0][0] + right[0][1]);
+        System.out.println(right[1][0] + right[1][1]);
+    }
 
     //Методы поворотов кубика (Язык поворотов кубика стандартный)
 
@@ -64,6 +91,28 @@ public class Cube {
             back[i][0] = down[i][0];
             down[i][0] = front[i][0];
             front[i][0] = turn[i];
+        }
+    }
+
+    // поворот задней грани на 90 градусов по часовой стрелке
+    public static void b() {
+        String[] turn = {up[0][0], up[0][1]};
+        for (int i = 0; i < 2; i++) {
+            up[0][i] = right[0][i];
+            right[0][i] = down[1][(i + 1) % 2];
+            down[1][(i + 1) % 2] = left[0][i];
+            left[0][i] = turn[i];
+        }
+    }
+
+    // поворот нижней грани на 90 градусов по часовой стрелке
+    public static void d() {
+        String[] turn = {front[1][0], front[1][1]};
+        for (int i = 0; i < 2; i++) {
+            front[1][i] = left[i][0];
+            left[i][0] = back[0][(i + 1) % 2];
+            back[0][(i + 1) % 2] = right[(i + 1) % 2][1];
+            right[(i + 1) % 2][1] = turn[i];
         }
     }
 }
