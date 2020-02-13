9a11d1761b0f47394c46228ef5a2b1cb5f3d87a5
Br0mm <Andrew-Ost@mail.ru>
2020-02-13 23:30:21 +0300

    Заменил грани кубика на enum. Добавил toString, equals, hashCode. Исправил комментарии преподавателя

--- src/MyProject/Cube.java
+++ src/MyProject/Cube.java
@@ -1,33 +1,47 @@
 package MyProject;
 
+import java.util.Arrays;
+import java.util.Objects;
+
+enum Colour{
+
+    RED,
+    WHITE,
+    ORANGE,
+    YELLOW,
+    BLUE,
+    GREEN
+}
+
 public class Cube {
 
-    private static String[][] front;
-    private static String[][] right;
-    private static String[][] left;
-    private static String[][] back;
-    private static String[][] up;
-    private static String[][] down;
-    private static int size;
+    private Colour[][] front;
+    private Colour[][] right;
+    private Colour[][] left;
+    private Colour[][] back;
+    private Colour[][] up;
+    private Colour[][] down;
+    private int size;
+    private final int repeats = 4;
 
-    Cube(int n) {
+    public Cube(int n) {
         if (n < 0) {
             throw new IllegalArgumentException();
         }
-        front = new String[n][n];
-        right = new String[n][n];
-        left = new String[n][n];
-        back = new String[n][n];
-        up = new String[n][n];
-        down = new String[n][n];
+        front = new Colour[n][n];
+        right = new Colour[n][n];
+        left = new Colour[n][n];
+        back = new Colour[n][n];
+        up = new Colour[n][n];
+        down = new Colour[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                front[i][j] = "\u001B[31m" + "▮"; //Red
-                right[i][j] = "\u001B[34m" + "▮"; //Blue
-                left[i][j] = "\u001B[32m" + "▮"; //Green
-                back[i][j] = "\u001B[35m" + "▮"; //Purple
-                up[i][j] = "\u001B[37m" + "▮"; //White
-                down[i][j] = "\u001B[33m" + "▮"; //Yellow
+                front[i][j] = Colour.RED;
+                right[i][j] = Colour.BLUE;
+                left[i][j] = Colour.GREEN;
+                back[i][j] = Colour.ORANGE;
+                up[i][j] = Colour.WHITE;
+                down[i][j] = Colour.YELLOW;
             }
         }
         size = n - 1;
@@ -38,11 +52,11 @@ public class Cube {
     //необходимо в метод передать его номер (от 0 до размера кубика - 1)
 
     //поворот грани, отсчитывая справа на 90 градусов по часовой стрелке
-    public static void r(int k) {
+    public void rightFaceClockwise(int k) {
         if (k > size || k < 0) {
             throw new IllegalArgumentException();
         }
-        String[] turn = new String[size + 1];
+        Colour[] turn = new Colour[size + 1];
         for (int i = 0; i < size + 1; i++) {
             turn[i] = up[i][size - k];
             up[i][size - k] = front[i][size - k];
@@ -50,21 +64,20 @@ public class Cube {
             down[i][size - k] = back[i][size - k];
             back[i][size - k] = turn[i];
         }
-        if (k == 0) {
-            right = rotate(right);
-        } else if (k == size) {
-            for (int i = 1; i < 4; i++) {
+        if (k == 0) right = rotate(right);
+        else if (k == size) {
+            for (int i = 1; i < repeats; i++) {
                 left = rotate(left);
             }
         }
     }
 
     //поворот грани, отсчитывая сверху на 90 градусов по часовой стрелке
-    public static void u(int k) {
+    public void upFaceClockwise(int k) {
         if (k > size || k < 0) {
             throw new IllegalArgumentException();
         }
-        String[] turn = new String[size + 1];
+        Colour[] turn = new Colour[size + 1];
         for (int i = 0; i < size + 1; i++) {
             turn[i] = front[k][i];
             front[k][i] = right[(i + 1) % (size + 1)][k];
@@ -72,21 +85,20 @@ public class Cube {
             back[size - k][(i + 1) % (size + 1)] = left[i][size - k];
             left[i][size - k] = turn[i];
         }
-        if (k == 0) {
-            up = rotate(up);
-        } else if (k == size) {
-            for (int i = 1; i < 4; i++) {
+        if (k == 0) up = rotate(up);
+        else if (k == size) {
+            for (int i = 1; i < repeats; i++) {
                 down = rotate(down);
             }
         }
     }
 
     //поворот грани, отсчитывая спереди на 90 градусов по часовой стрелке
-    public static void f(int k) {
+    public void frontFaceClockwise(int k) {
         if (k > size || k < 0) {
             throw new IllegalArgumentException();
         }
-        String[] turn = new String[size + 1];
+        Colour[] turn = new Colour[size + 1];
         for (int i = 0; i < size + 1; i++) {
             turn[i] = up[size - k][i];
             up[size - k][i] = left[size - k][i];
@@ -94,115 +106,224 @@ public class Cube {
             down[k][(i + 1) % (size + 1)] = right[size - k][i];
             right[size - k][i] = turn[i];
         }
-        if (k == 0) {
-            front = rotate(front);
-        } else if (k == size) {
-            for (int i = 1; i < 4; i++) {
+        if (k == 0) front = rotate(front);
+        else if (k == size) {
+            for (int i = 1; i < repeats; i++) {
                 back = rotate(back);
             }
         }
     }
 
     // поворот грани, отсчитывая слева на 90 градусов по часовой стрелке
-    public static void l(int k) {
-        for (int i = 1; i < 4; i++) {
-            r(size - k);
+    public void leftFaceClockwise(int k) {
+        for (int i = 1; i < repeats; i++) {
+            rightFaceClockwise(size - k);
         }
     }
 
     // поворот грани, отсчитывая сзади на 90 градусов по часовой стрелке
-    public static void b(int k) {
-        for (int i = 1; i < 4; i++) {
-            f(size - k);
+    public void backFaceClockwise(int k) {
+        for (int i = 1; i < repeats; i++) {
+            frontFaceClockwise(size - k);
         }
     }
 
     // поворот грани, отсчитывая снизу на 90 градусов по часовой стрелке
-    public static void d(int k) {
-        for (int i = 1; i < 4; i++) {
-            u(size - k);
+    public void downFaceClockwise(int k) {
+        for (int i = 1; i < repeats; i++) {
+            upFaceClockwise(size - k);
         }
     }
 
 
     //Повороты кубика
     //Поворот кубика влево
-    public static void turnLeft() {
+    public void turnLeft() {
         for (int i = 0; i < size + 1; i++) {
-            u(i);
+            upFaceClockwise(i);
         }
     }
 
     //Поворот кубика вправо
-    public static void turnRight() {
+    public void turnRight() {
         for (int i = 0; i < size + 1; i++) {
-            d(i);
+            downFaceClockwise(i);
         }
     }
 
     //Поворот кубика вверх
-    public static void turnUp() {
+    public void turnUp() {
         for (int i = 0; i < size + 1; i++) {
-            r(i);
+            rightFaceClockwise(i);
         }
     }
 
     //Поворот кубика вниз
-    public static void turnDown() {
+    public void turnDown() {
         for (int i = 0; i < size + 1; i++) {
-            l(i);
+            leftFaceClockwise(i);
         }
     }
 
     //Поворот кубика по часовой стрелке
-    public static void turnClockwise() {
+    public void turnClockwise() {
         for (int i = 0; i < size + 1; i++) {
-            f(i);
+            frontFaceClockwise(i);
         }
     }
 
     //Поворот кубика по часовой стрелке
-    public static void turnCounterClockwise() {
+    public void turnCounterClockwise() {
         for (int i = 0; i < size + 1; i++) {
-            b(i);
+            backFaceClockwise(i);
         }
     }
 
     //запрос состояния грани
-    public static String[][] status() {
+    public Colour[][] status() {
         return front;
     }
 
     //случайная установка состояния кубика
-    public static void shuffle() {
+    public void shuffle() {
         int n = (int) (Math.random() * 3);
         for (int i = 1; i < 3 + n; i++) {
             int k = (int) (Math.random() * 3);
             for (int j = 0; j < k; j++) {
-                r(0);
-                d(0);
+                rightFaceClockwise(0);
+                downFaceClockwise(0);
             }
             k = (int) (Math.random() * 3);
             for (int j = 0; j < k; j++) {
-                b(0);
-                l(0);
+                backFaceClockwise(0);
+                leftFaceClockwise(0);
             }
             k = (int) (Math.random() * 3);
             for (int j = 0; j < k; j++) {
-                u(0);
-                f(0);
+                upFaceClockwise(0);
+                frontFaceClockwise(0);
             }
         }
     }
 
     //поворот содержимого грани на 90 градусов по часовой стрелке
-    private static String[][] rotate (String[][] face) {
-        String[][] result = new String[face[0].length][face[0].length];
+    private Colour[][] rotate (Colour[][] face) {
+        Colour[][] result = new Colour[face[0].length][face[0].length];
         for (int i = 0; i < face[0].length; i++) {
             for (int j = face[0].length - 1; j >= 0; j--) {
                 result[i][face[0].length - 1 - j] = face[j][i];
             }
         }
         return result;
+    }
+
+    private Boolean equalsOfFaces(Cube other, Colour[][] Face) {
+        boolean flag = false;
+        for (int i = 1; i < repeats + 1; i++) {
+            if (Arrays.deepEquals(other.status(), Face)) flag = true;
+            else other.turnClockwise();
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Cube other = (Cube) obj;
+        Boolean flag = false;
+        other.turnUp();
+        for (int i = 1; i < repeats + 1; i++) {
+            if (!flag) {
+                other.turnDown();
+                flag = equalsOfFaces(other, front);
+            }
+        }
+        if (!flag) {
+            other.turnDown();
+            other.turnLeft();
+            flag = equalsOfFaces(other, front);
+        }
+        if (!flag) {
+            other.turnRight();
+            other.turnRight();
+            flag = equalsOfFaces(other, front);
+        }
+        if (!flag) other.turnLeft();
+        if (flag) {
+            other.turnCounterClockwise();
+            for (int i = 1; i < repeats + 1; i++) {
+                other.turnClockwise();
+                if (Arrays.deepEquals(other.up, up)) {
+                    return Arrays.deepEquals(other.back, back) && Arrays.deepEquals(other.down, down)
+                            && Arrays.deepEquals(other.right, right) && Arrays.deepEquals(other.left, left);
+                }
+            }
+            return false;
+        } else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size);
+        result = 31 * result + Arrays.hashCode(front);
+        result = 31 * result + Arrays.hashCode(right);
+        result = 31 * result + Arrays.hashCode(left);
+        result = 31 * result + Arrays.hashCode(back);
+        result = 31 * result + Arrays.hashCode(up);
+        result = 31 * result + Arrays.hashCode(down);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        String face;
+        final StringBuilder sb = new StringBuilder("Cube{\n");
+        turnUp();
+        for (int j = 1; j < repeats + 1; j++) {
+            turnDown();
+            switch (j) {
+                case 1:
+                    face = "Front";
+                    break;
+                case 2:
+                    face = "Up";
+                    break;
+                case 3:
+                    face = "Back";
+                    turnClockwise();
+                    turnClockwise();
+                    break;
+                default:
+                    turnUp();
+                    turnClockwise();
+                    turnClockwise();
+                    turnDown();
+                    face = "Down";
+            }
+            sb.append(face).append(" \n");
+            for (int i = 0; i < size + 1; i++) {
+                sb.append(Arrays.toString(front[i]));
+                sb.append('\n');
+            }
+        }
+        turnDown();
+        turnLeft();
+        sb.append("Right").append(" \n");
+        for (int i = 0; i < size + 1; i++) {
+            sb.append(Arrays.toString(front[i]));
+            sb.append('\n');
+        }
+        turnRight();
+        turnRight();
+        sb.append("Left").append(" \n");
+        for (int i = 0; i < size + 1; i++) {
+            sb.append(Arrays.toString(front[i]));
+            sb.append('\n');
+        }
+        sb.append("}");
+
+        return sb.toString();
     }
 }
--- test/MyProject/CubeTest.java
+++ test/MyProject/CubeTest.java
@@ -7,183 +7,203 @@ import org.junit.jupiter.api.Test;
 class CubeTest {
     Cube duo = new Cube(2);
 
+    //тесты для проверки правильности поворота отдельных граней
     @Test
     void turns() {
-        String[][] side = {{"\u001B[31m" + "▮", "\u001B[33m" + "▮"}, {"\u001B[31m" + "▮", "\u001B[33m" + "▮"}};
-        Cube.r(0);
-        Assertions.assertArrayEquals(Cube.status(), side);
-        side[0][0] = "\u001B[33m" + "▮";
-        side[1][0] = "\u001B[33m" + "▮";
-        Cube.r(1);
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.l(0);
-        Assertions.assertNotEquals(Cube.status(), side);
-        Cube.l(1);
+        Colour[][] side = {{Colour.RED, Colour.YELLOW}, {Colour.RED, Colour.YELLOW}};
+        duo.rightFaceClockwise(0);
+        Assertions.assertArrayEquals(duo.status(), side);
+        side[0][0] = Colour.YELLOW;
+        side[1][0] = Colour.YELLOW;
+        duo.rightFaceClockwise(1);
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.leftFaceClockwise(0);
+        Assertions.assertNotEquals(duo.status(), side);
+        duo.leftFaceClockwise(1);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[31m" + "▮";
+                side[i][j] = Colour.RED;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.u(0);
-        Assertions.assertNotEquals(Cube.status(), side);
-        Cube.u(1);
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.upFaceClockwise(0);
+        Assertions.assertNotEquals(duo.status(), side);
+        duo.upFaceClockwise(1);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[34m" + "▮";
+                side[i][j] = Colour.BLUE;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.d(0);
-        side[1][0] = "\u001B[31m" + "▮";
-        side[1][1] = "\u001B[31m" + "▮";
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.d(1);
-        Assertions.assertNotEquals(Cube.status(), side);
-        side[0][0] = "\u001B[31m" + "▮";
-        side[0][1] = "\u001B[31m" + "▮";
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnClockwise();
-        Cube.turnRight();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.downFaceClockwise(0);
+        side[1][0] = Colour.RED;
+        side[1][1] = Colour.RED;
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.downFaceClockwise(1);
+        Assertions.assertNotEquals(duo.status(), side);
+        side[0][0] = Colour.RED;
+        side[0][1] = Colour.RED;
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnClockwise();
+        duo.turnRight();
+        Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[33m" + "▮";
+                side[i][j] = Colour.YELLOW;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnLeft();
-        Cube.turnCounterClockwise();
-        Cube.turnRight();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnLeft();
+        duo.turnCounterClockwise();
+        duo.turnRight();
+        Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[32m" + "▮";
+                side[i][j] = Colour.GREEN;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnLeft();
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnLeft();
     }
 
+    //тесты для проверки правильности поворотов кубика
     @Test
     void flips() {
-        String[][] side = {{"\u001B[34m" + "▮", "\u001B[34m" + "▮"}, {"\u001B[34m" + "▮", "\u001B[34m" + "▮"}};
-        Cube.turnLeft();
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnLeft();
-        Assertions.assertNotEquals(Cube.status(), side);
-        Cube.turnDown();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Colour[][] side = {{Colour.BLUE, Colour.BLUE}, {Colour.BLUE, Colour.BLUE}};
+        duo.turnLeft();
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnLeft();
+        Assertions.assertNotEquals(duo.status(), side);
+        duo.turnDown();
+        Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[37m" + "▮";
+                side[i][j] = Colour.WHITE;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnUp();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnUp();
+        Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[35m" + "▮";
+                side[i][j] = Colour.ORANGE;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnRight();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnRight();
+        Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[34m" + "▮";
+                side[i][j] = Colour.BLUE;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnRight();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnRight();
+        Assertions.assertNotEquals(duo.status(), side);
     }
 
+    //тесты для проверки правильности поворотов передней и задней грани
     @Test
     void frontTurns() {
-        String[][] side = {{"\u001B[31m" + "▮", "\u001B[31m" + "▮"}, {"\u001B[31m" + "▮", "\u001B[31m" + "▮"}};
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.f(0);
-        Cube.turnLeft();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Colour[][] side = {{Colour.RED, Colour.RED}, {Colour.RED, Colour.RED}};
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.frontFaceClockwise(0);
+        duo.turnLeft();
+        Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[34m" + "▮";
+                side[i][j] = Colour.BLUE;
             }
         }
-        Assertions.assertNotEquals(Cube.status(), side);
-        side[0][0] = "\u001B[37m" + "▮";
-        side[1][0] = "\u001B[37m" + "▮";
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnRight();
-        Cube.b(0);
-        Cube.turnLeft();
-        Assertions.assertNotEquals(Cube.status(), side);
-        side[0][1] = "\u001B[33m" + "▮";
-        side[1][1] = "\u001B[33m" + "▮";
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnRight();
-        Cube.b(1);
-        Cube.f(1);
-        Cube.turnLeft();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Assertions.assertNotEquals(duo.status(), side);
+        side[0][0] = Colour.WHITE;
+        side[1][0] = Colour.WHITE;
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnRight();
+        duo.backFaceClockwise(0);
+        duo.turnLeft();
+        Assertions.assertNotEquals(duo.status(), side);
+        side[0][1] = Colour.YELLOW;
+        side[1][1] = Colour.YELLOW;
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnRight();
+        duo.backFaceClockwise(1);
+        duo.frontFaceClockwise(1);
+        duo.turnLeft();
+        Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[34m" + "▮";
+                side[i][j] = Colour.BLUE;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.turnRight();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.turnRight();
+        Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                side[i][j] = "\u001B[31m" + "▮";
+                side[i][j] = Colour.RED;
             }
         }
-        Assertions.assertArrayEquals(Cube.status(), side);
+        Assertions.assertArrayEquals(duo.status(), side);
     }
 
+    //тесты для проверки что перемешивание кубика работает правильно
     @Test
     void randomPosition() {
-        String[][] side = {{"\u001B[31m" + "▮", "\u001B[31m" + "▮"}, {"\u001B[31m" + "▮", "\u001B[31m" + "▮"}};
-        Assertions.assertArrayEquals(Cube.status(), side);
-        Cube.shuffle();
-        Assertions.assertNotEquals(Cube.status(), side);
-        Cube.turnLeft();
-        Assertions.assertNotEquals(Cube.status(), side);
-        Cube.shuffle();
-        Assertions.assertNotEquals(Cube.status(), side);
-        side = Cube.status();
-        Cube.shuffle();
-        Assertions.assertNotEquals(Cube.status(), side);
+        Colour[][] side = {{Colour.RED, Colour.RED}, {Colour.RED, Colour.RED}};
+        Assertions.assertArrayEquals(duo.status(), side);
+        duo.shuffle();
+        Assertions.assertNotEquals(duo.status(), side);
+        duo.turnLeft();
+        Assertions.assertNotEquals(duo.status(), side);
+        duo.shuffle();
+        Assertions.assertNotEquals(duo.status(), side);
+        side = duo.status();
+        duo.shuffle();
+        Assertions.assertNotEquals(duo.status(), side);
     }
 
+    //тесты для проверки правильности выбрасывания исключений
     @Test
     void illegalArguments() {
         try {
-            Cube.r(3);
+            duo.rightFaceClockwise(3);
             Assertions.fail();
         } catch (IllegalArgumentException e) {
-            assert true;
         }
         try {
-            Cube.u(3);
+            duo.upFaceClockwise(3);
             Assertions.fail();
         } catch (IllegalArgumentException e) {
-            assert true;
         }
         try {
-            Cube.f(3);
+            duo.frontFaceClockwise(3);
             Assertions.fail();
         } catch (IllegalArgumentException e) {
-            assert true;
         }
         try {
             Cube a = new Cube (-1);
             Assertions.fail();
         } catch (IllegalArgumentException e) {
-            assert true;
         }
+    }
+
+    //тесты для проверки правильности equals()
+    @Test
+    void equalsTest() {
+        Cube a = new Cube(2);
+        Cube b = new Cube(2);
+        Assertions.assertEquals(a, b); //проверка что два одинаковых кубика равны
+        a.turnUp(); //поворачивем один из кубиков
+        Assertions.assertEquals(a, b); //проверяем что кубики всё равно равны, не смотря на поворот
+        a.turnDown(); //возвращаем в изначальное положение
+        a.rightFaceClockwise(0); //поворачиваем одну из граней
+        a.turnClockwise(); //делаем несколько поворотов всего кубика
+        a.turnDown();
+        b.rightFaceClockwise(0); // поворачиваем ту же грань что и у кубика a
+        Assertions.assertEquals(a, b); ////проверяем что кубики всё равно равны, не смотря на манипуляции
+        b.rightFaceClockwise(0);
+        Assertions.assertNotEquals(a, b);
+        b.leftFaceClockwise(1);
     }
 }
