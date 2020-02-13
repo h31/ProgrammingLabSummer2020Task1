fe91dfb255acef5569bfef18b2aeb5c07a462e41
Br0mm <Andrew-Ost@mail.ru>
2020-02-14 00:09:49 +0300

    Заменил грани кубика на enum. Добавил toString, equals, hashCode. Исправил комментарии преподавателя

--- test/MyProject/CubeTest.java
+++ test/MyProject/CubeTest.java
@@ -10,13 +10,16 @@ class CubeTest {
     //тесты для проверки правильности поворота отдельных граней
     @Test
     void turns() {
+        //поворачиваю грань кубика и сравниваю с ожидаемым результатом
         Colour[][] side = {{Colour.RED, Colour.YELLOW}, {Colour.RED, Colour.YELLOW}};
         duo.rightFaceClockwise(0);
         Assertions.assertArrayEquals(duo.status(), side);
+        //проверяю как работает поворот с другим аргументом
         side[0][0] = Colour.YELLOW;
         side[1][0] = Colour.YELLOW;
         duo.rightFaceClockwise(1);
         Assertions.assertArrayEquals(duo.status(), side);
+        //аналогичные действия для другого метода поворота грани
         duo.leftFaceClockwise(0);
         Assertions.assertNotEquals(duo.status(), side);
         duo.leftFaceClockwise(1);
@@ -26,6 +29,7 @@ class CubeTest {
             }
         }
         Assertions.assertArrayEquals(duo.status(), side);
+        //аналогично
         duo.upFaceClockwise(0);
         Assertions.assertNotEquals(duo.status(), side);
         duo.upFaceClockwise(1);
@@ -35,6 +39,7 @@ class CubeTest {
             }
         }
         Assertions.assertArrayEquals(duo.status(), side);
+        //аналогично
         duo.downFaceClockwise(0);
         side[1][0] = Colour.RED;
         side[1][1] = Colour.RED;
@@ -44,6 +49,7 @@ class CubeTest {
         side[0][0] = Colour.RED;
         side[0][1] = Colour.RED;
         Assertions.assertArrayEquals(duo.status(), side);
+        //несколько поворотов всего кубика для проверки правильности поворотов граней
         duo.turnClockwise();
         duo.turnRight();
         Assertions.assertNotEquals(duo.status(), side);
@@ -69,11 +75,13 @@ class CubeTest {
     //тесты для проверки правильности поворотов кубика
     @Test
     void flips() {
+        //поворачиваю весь кубик и сравниваю с ожидаемым результатом
         Colour[][] side = {{Colour.BLUE, Colour.BLUE}, {Colour.BLUE, Colour.BLUE}};
         duo.turnLeft();
         Assertions.assertArrayEquals(duo.status(), side);
         duo.turnLeft();
         Assertions.assertNotEquals(duo.status(), side);
+        //аналогично, но поворот в другую сторону
         duo.turnDown();
         Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
@@ -82,6 +90,7 @@ class CubeTest {
             }
         }
         Assertions.assertArrayEquals(duo.status(), side);
+        //поворот в другую сторону
         duo.turnUp();
         Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
@@ -90,6 +99,7 @@ class CubeTest {
             }
         }
         Assertions.assertArrayEquals(duo.status(), side);
+        //поворот в другую сторону
         duo.turnRight();
         Assertions.assertNotEquals(duo.status(), side);
         for (int i = 0; i < 2; i++) {
@@ -105,21 +115,23 @@ class CubeTest {
     //тесты для проверки правильности поворотов передней и задней грани
     @Test
     void frontTurns() {
+        //поворачиваю переднюю грань, затем поворачиваю кубик, чтобы проверить правильность
         Colour[][] side = {{Colour.RED, Colour.RED}, {Colour.RED, Colour.RED}};
         Assertions.assertArrayEquals(duo.status(), side);
         duo.frontFaceClockwise(0);
         duo.turnLeft();
         Assertions.assertNotEquals(duo.status(), side);
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) { //задаю значение грани которое было бы без поворота передней грани
             for (int j = 0; j < 2; j++) {
                 side[i][j] = Colour.BLUE;
             }
         }
-        Assertions.assertNotEquals(duo.status(), side);
+        Assertions.assertNotEquals(duo.status(), side); //проверяю, что не должно сходиться
         side[0][0] = Colour.WHITE;
         side[1][0] = Colour.WHITE;
-        Assertions.assertArrayEquals(duo.status(), side);
+        Assertions.assertArrayEquals(duo.status(), side); // задаю как должно быть и проверяю
         duo.turnRight();
+        //аналогично, но поворачиваю заднюю грань
         duo.backFaceClockwise(0);
         duo.turnLeft();
         Assertions.assertNotEquals(duo.status(), side);
@@ -127,6 +139,7 @@ class CubeTest {
         side[1][1] = Colour.YELLOW;
         Assertions.assertArrayEquals(duo.status(), side);
         duo.turnRight();
+        //проверяю эти методы с другими аргументами
         duo.backFaceClockwise(1);
         duo.frontFaceClockwise(1);
         duo.turnLeft();
