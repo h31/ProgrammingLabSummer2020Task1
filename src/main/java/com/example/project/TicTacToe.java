9414d7fb0d898ffe8fe280c3b34f9b5644a26859
somagic2 <nik.p008@gmail.com>
2020-03-05 10:23:07 +0300

    Исправлены замечания

--- src/main/java/com/example/project/TicTacToe.java
+++ src/main/java/com/example/project/TicTacToe.java
@@ -4,256 +4,215 @@ package com.example.project;
 import java.util.Arrays;
 import java.util.Objects;
 
+enum TicTacSymbol {
+    CROSS,
+    ZERO,
+    EMPTY
+}
+
+enum DirectionSymbol {
+    VERTICAL,
+    HORIZONTAL,
+    DIAGONAL
+}
+
+final class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
+
+final class Line {
+    private final int length;
+    private final Point point1;
+    private final Point point2;
+
+    public Line(int length, Point point1, Point point2) {
+        this.length = length;
+        this.point1 = point1;
+        this.point2 = point2;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return length == line.length &&
+                point1.equals(line.point1) &&
+                point2.equals(line.point2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(length, point1, point2);
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "length=" + length +
+                ", point1=" + point1 +
+                ", point2=" + point2 +
+                '}';
+    }
+}
+
 public final class TicTacToe {
     private final int size;
-    public String[][] desk; // поле хранится в виде двухмерного массива
+    public char[][] desk; // поле хранится в виде двухмерного массива
 
     public TicTacToe(int size) { // конструктор
         this.size = size;
-        this.desk = new String[size][size];
+        this.desk = new char[size][size];
         for (int i = 0; i < size; i++) // заполнение поля по умолчанию
             for (int j = 0; j < size; j++)
-                this.desk[i][j] = "*";
+                this.desk[i][j] = '*';
     }
 
     // методы
     public void addCross(int x, int y) { // добавить крестик
-        desk[x][y] = "X";
+        if ((x < 0) || (x > size - 1) || (y < 0) || (y > size - 1)) throw new IllegalArgumentException();
+        desk[x][y] = 'X';
     }
 
     public void addZero(int x, int y) { // добавить нолик
-        desk[x][y] = "O";
+        if ((x < 0) || (x > size - 1) || (y < 0) || (y > size - 1)) throw new IllegalArgumentException();
+        desk[x][y] = 'O';
     }
 
     public void cleanCell(int x, int y) { // очистить заданную клетку
-        desk[x][y] = "*";
+        desk[x][y] = '*';
     }
 
-    public String[][] getDesk() { // запрос текущего состояния поля
+    public char[][] getDesk() { // запрос текущего состояния поля
         return this.desk;
     }
 
-    public String getFieldAt(int x, int y) { // получение конкретного элемента поля по координатам
+    public char getFieldAt(int x, int y) { // получение конкретного элемента поля по координатам
         return this.desk[x][y];
     }
 
-    public String findVertical(String symbol) {
-        int i, j;
-        int cnt;
+    public Line getWinner(TicTacSymbol symbol, DirectionSymbol direction) {
+        char findSymbol = '*';
         int max = 0;
-        boolean firstCase;
-        int startx = -1;
-        int starty = -1;
-        int maxStartx = -1;
-        int maxStarty = -1;
-        int endx = -1;
-        int endy = -1;
-        String str;
-        for (j = 0; j < size; j++) {
-            cnt = 0;
-            firstCase = true;
-            for (i = 0; i < size; i++) {
-                if (desk[i][j].equals(symbol)) {
-                    if (firstCase) {
-                        firstCase = false;
-                        startx = i;
-                        starty = j;
-                    }
-                    if (i == size - 1) {
-                        cnt++;
-                        if (cnt > max) {
-                            max = cnt;
-                            endx = i;
-                            endy = j;
-                            maxStartx = startx;
-                            maxStarty = starty;
-                        }
-                        cnt = 0;
-                    } else cnt++;
-                }
-                if (!desk[i][j].equals(symbol)) {
-                    firstCase = true;
-                    if (cnt > max) {
-                        max = cnt;
-                        endx = i - 1;
-                        endy = j;
-                        maxStartx = startx;
-                        maxStarty = starty;
-                    }
-                    cnt = 0;
-                }
-            }
+        Line answer = new Line(0, new Point(0, 0), new Point(0, 0));
+        Line result = new Line(0, new Point(0, 0), new Point(0, 0));
+        switch (symbol) {
+            case CROSS:
+                findSymbol = 'X';
+                break;
+            case ZERO:
+                findSymbol = 'O';
+                break;
+            case EMPTY:
+                findSymbol = '*';
+                break;
         }
-        str = "максимальная длина " + max + " координаты начала (" + maxStartx + ", " + maxStarty + ") "
-                + "координаты конца (" + endx + ", " + endy + ")";
-        return str;
+        switch (direction) {
+            case VERTICAL:
+                for (int i = 0; i < size; i++) {
+                    result = check(findSymbol, 1, 0, 0, i);
+                    if (result.getLength() > max) {
+                        max = result.getLength();
+                        answer = result;
+                    }
+                }
+                break;
+            case HORIZONTAL:
+                for (int i = 0; i < size; i++) {
+                    result = check(findSymbol, 0, i, 1, 0);
+                    if (result.getLength() > max) {
+                        max = result.getLength();
+                        answer = result;
+                    }
+                }
+                break;
+            case DIAGONAL:
+                for (int i = 0; i < size; i++) {
+                    Line result1 = check(findSymbol, 1, 0, 1, 0);
+                    Line result2 = check(findSymbol, -1, size - 1, 1, 0);
+                    answer = (result1.getLength() > result2.getLength()) ? result1 : result2;
+                }
+                break;
+        }
+        return answer;
     }
 
-    public String findHorizontal(String symbol) {
-        int i, j;
-        int cnt;
-        int max = 0;
-        boolean firstCase;
-        int startx = -1;
-        int starty = -1;
-        int maxStartx = -1;
-        int maxStarty = -1;
-        int endx = -1;
-        int endy = -1;
-        String str;
-        for (i = 0; i < size; i++) {
-            cnt = 0;
-            firstCase = true;
-            for (j = 0; j < size; j++) {
-                if (desk[i][j].equals(symbol)) {
-                    if (firstCase) {
-                        firstCase = false;
-                        startx = i;
-                        starty = j;
-                    }
-                    if (j == size - 1) {
-                        cnt++;
-                        if (cnt > max) {
-                            max = cnt;
-                            endx = i;
-                            endy = j;
-                            maxStartx = startx;
-                            maxStarty = starty;
-                        }
-                        cnt = 0;
-                    } else cnt++;
-                }
-                if (!desk[i][j].equals(symbol)) {
-                    firstCase = true;
-                    if (cnt > max) {
-                        max = cnt;
-                        endx = i; // меняется для горизонтали
-                        endy = j - 1;
-                        maxStartx = startx;
-                        maxStarty = starty;
-                    }
-                    cnt = 0;
-                }
-            }
-        }
-        str = "максимальная длина " + max + " координаты начала (" + maxStartx + ", " + maxStarty + ") "
-                + "координаты конца (" + endx + ", " + endy + ")";
-        return str;
-    }
-
-    public String findDiagonal(String symbol) {
-        int i, j;
+    private Line check(char findSymbol, int xCoef, int xShift, int yCoef, int yShift) {
+        int i = 0;
         int cnt = 0;
-        int max1 = 0;
-        int max2 = 0;
-        int maximum;
-        boolean firstCase = true;
-        int startx1 = -1;
-        int starty1 = -1;
-        int startx2 = -1;
-        int starty2 = -1;
-        int maxStartx1 = -1;
-        int maxStarty1 = -1;
-        int maxStartx2 = -1;
-        int maxStarty2 = -1;
-        int maxStartx;
-        int maxStarty;
-        int endx1 = -1;
-        int endy1 = -1;
-        int endx2 = -1;
-        int endy2 = -1;
-        int endx;
-        int endy;
-        String str;
+        int max = 0;
+        Point startPoint = new Point(-1, -1);
+        Point endPoint = new Point(-1, -1);
+        Point maxStartPoint = new Point(-1, -1);
+        Point maxEndPoint = new Point(-1, -1);
+        int previousx = i * xCoef + xShift;
+        int previousy = i * yCoef + yShift;
         for (i = 0; i < size; i++) {
-            for (j = 0; j < size; j++) {
-                if (i == j) { // поиск по главной диагонали
-                    if (desk[i][j].equals(symbol)) {
-                        if (firstCase) {
-                            firstCase = false;
-                            startx1 = i;
-                            starty1 = j;
-                        }
-                        if (j == size - 1) {
-                            cnt++;
-                            if (cnt > max1) {
-                                max1 = cnt;
-                                endx1 = i;
-                                endy1 = j;
-                                maxStartx1 = startx1;
-                                maxStarty1 = starty1;
-                            }
-                            cnt = 0;
-                        } else cnt++;
-                    }
-                    if (!desk[i][j].equals(symbol)) {
-                        firstCase = true;
-                        if (cnt > max1) {
-                            max1 = cnt;
-                            endx1 = i - 1; // меняется для горизонтали
-                            endy1 = j - 1;
-                            maxStartx1 = startx1;
-                            maxStarty1 = starty1;
-                        }
-                        cnt = 0;
+            int x = i * xCoef + xShift;
+            int y = i * yCoef + yShift;
+            char symbol = desk[x][y];
+            if (symbol == findSymbol) {
+                if (cnt == 0) {
+                    startPoint = new Point(x, y);
+                }
+                cnt++;
+                if (i == size - 1) {
+                    endPoint = new Point(x, y);
+                    if (cnt > max) {
+                        max = cnt;
+                        maxStartPoint = startPoint;
+                        maxEndPoint = endPoint;
                     }
                 }
-            }
-        }
-        cnt = 0;
-        firstCase = true;
-
-        for (i = 0; i < size; i++) {
-            for (j = 0; j < size; j++) {
-                if (i + j == size - 1) { // поиск по побочной диагонали
-                    if (desk[i][j].equals(symbol)) {
-                        if (firstCase) {
-                            firstCase = false;
-                            startx2 = i;
-                            starty2 = j;
-                        }
-                        if (i == size - 1) {
-                            cnt++;
-                            if (cnt > max2) {
-                                max2 = cnt;
-                                endx2 = i;
-                                endy2 = j;
-                                maxStartx2 = startx2;
-                                maxStarty2 = starty2;
-                            }
-                            cnt = 0;
-                        } else cnt++;
-                    }
-                    if (!desk[i][j].equals(symbol)) {
-                        firstCase = true;
-                        if (cnt > max2) {
-                            max2 = cnt;
-                            endx2 = i - 1; // меняется для горизонтали
-                            endy2 = j + 1;
-                            maxStartx2 = startx2;
-                            maxStarty2 = starty2;
-                        }
-                        cnt = 0;
-                    }
+            } else {
+                if ((cnt != 0) && (i == size - 1)) {
+                    endPoint = new Point(previousx, previousy);
+                } else if (cnt != 0) {
+                    endPoint = new Point(previousx, previousy);
                 }
+                if (cnt > max) {
+                    max = cnt;
+                    maxStartPoint = startPoint;
+                    maxEndPoint = endPoint;
+                }
+                cnt = 0;
             }
+            previousx = x;
+            previousy = y;
         }
-        maximum = Math.max(max1, max2);
-        if (maximum == max1) {
-            maxStartx = maxStartx1;
-            maxStarty = maxStarty1;
-            endx = endx1;
-            endy = endy1;
-        } else {
-            maxStartx = maxStartx2;
-            maxStarty = maxStarty2;
-            endx = endx2;
-            endy = endy2;
-        }
-
-        str = "максимальная длина " + maximum + " координаты начала (" + maxStartx + ", " + maxStarty + ") "
-                + "координаты конца (" + endx + ", " + endy + ")";
-        return str;
+        return new Line(max, maxStartPoint, maxEndPoint);
     }
 
     @Override
@@ -272,5 +231,17 @@ public final class TicTacToe {
         int result = Objects.hash(size);
         result = 31 * result + Arrays.hashCode(desk);
         return result;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                str += desk[i][j] + " ";
+            }
+            str += "\n";
+        }
+        return str;
     }
 }
--- src/test/java/com/example/project/TicTacToeTests.java
+++ src/test/java/com/example/project/TicTacToeTests.java
@@ -2,8 +2,7 @@ package com.example.project;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TicTacToeTests {
@@ -11,7 +10,7 @@ public class TicTacToeTests {
     public void addCross() {
         TicTacToe game = new TicTacToe(3);
         game.addCross(2, 0);
-        assertEquals("X", game.getFieldAt(2, 0));
+        assertEquals('X', game.getFieldAt(2, 0));
     }
 
     @Test
@@ -19,8 +18,8 @@ public class TicTacToeTests {
         TicTacToe game = new TicTacToe(3);
         game.addZero(1, 0);
         game.addZero(2, 0);
-        assertEquals("O", game.getFieldAt(1, 0));
-        assertEquals("O", game.getFieldAt(2, 0));
+        assertEquals('O', game.getFieldAt(1, 0));
+        assertEquals('O', game.getFieldAt(2, 0));
     }
 
     @Test
@@ -28,14 +27,13 @@ public class TicTacToeTests {
         TicTacToe game = new TicTacToe(3);
         game.addCross(0, 2);
         game.cleanCell(0, 2);
-        assertEquals("*", game.getFieldAt(0, 2));
+        assertEquals('*', game.getFieldAt(0, 2));
     }
 
 
     @Test
     public void getDesk() {
         TicTacToe game = new TicTacToe(3);
-        game.getDesk();
         assertTrue(game.desk.equals(game.getDesk()));
     }
 
@@ -43,45 +41,28 @@ public class TicTacToeTests {
     public void getFieldAt() {
         TicTacToe game = new TicTacToe(3);
         game.addCross(0,1);
-        assertEquals("X", game.getFieldAt(0, 1));
-        assertEquals("*", game.getFieldAt(1, 0));
+        assertEquals('X', game.getFieldAt(0, 1));
+        assertEquals('*', game.getFieldAt(1, 0));
     }
 
     @Test
-    public void findVertical () {
+    public void getWinner() {
         TicTacToe game = new TicTacToe(4);
         game.addCross(0, 1);
         game.addCross(1, 1);
         game.addCross(2, 1);
         game.addZero(0,0);
         game.addZero(1, 0);
-        assertEquals("максимальная длина 3 координаты начала (0, 1) координаты конца (2, 1)", game.findVertical("X"));
-        assertEquals("максимальная длина 2 координаты начала (0, 0) координаты конца (1, 0)", game.findVertical("O"));
-    }
-
-    @Test
-    public void findHorizontal() {
-        TicTacToe game = new TicTacToe(4);
-        game.addCross(0, 0);
-        game.addCross(0, 1);
-        game.addCross(0, 2);
-        game.addZero(3, 0);
-        game.addZero(3,1);
-        assertEquals("максимальная длина 3 координаты начала (0, 0) координаты конца (0, 2)", game.findHorizontal("X"));
-        assertEquals("максимальная длина 2 координаты начала (3, 0) координаты конца (3, 1)", game.findHorizontal("O"));
-    }
-
-    @Test
-    public void findDiagonal() {
-        TicTacToe game = new TicTacToe(4);
-        game.addCross(0, 0);
-        game.addCross(1, 1);
-        game.addCross(2, 2);
-        game.addZero(3, 0);
-        game.addZero(2, 1);
-        game.addZero(1, 2);
-        game.addZero(0, 3);
-        assertEquals("максимальная длина 3 координаты начала (0, 0) координаты конца (2, 2)", game.findDiagonal("X"));
-        assertEquals("максимальная длина 4 координаты начала (0, 3) координаты конца (3, 0)", game.findDiagonal("O"));
+        game.addCross(3, 0);
+        game.addZero(3,2);
+        game.addCross(0,2);
+        game.addCross(0, 3);
+        game.addZero(3,3);
+        game.addZero(2,2);
+        assertEquals(new Line(3, new Point(0, 1), new Point(2, 1)), game.getWinner(TicTacSymbol.CROSS, DirectionSymbol.VERTICAL));
+        assertEquals(new Line(2, new Point(0, 0), new Point(1, 0)), game.getWinner(TicTacSymbol.ZERO, DirectionSymbol.VERTICAL));
+        assertEquals(new Line(2, new Point(3, 2), new Point(3, 3)), game.getWinner(TicTacSymbol.ZERO, DirectionSymbol.HORIZONTAL));
+        assertEquals(new Line(2, new Point(3, 0), new Point(2, 1)), game.getWinner(TicTacSymbol.CROSS, DirectionSymbol.DIAGONAL));
+        assertEquals(new Line(2, new Point(2, 2), new Point(3, 3)), game.getWinner(TicTacSymbol.ZERO, DirectionSymbol.DIAGONAL));
     }
 }
